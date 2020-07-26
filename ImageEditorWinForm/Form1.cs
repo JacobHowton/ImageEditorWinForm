83e1ec51004ba7443ed9ddfc9d6181414c7f6d3d
jacob <jacobhwtn@gmail.com>
2020-07-25 22:43:10 -0500

    Added a picture box and starting thinking how to change / store pixles

--- ImageEditorWinForm/Form1.cs
+++ ImageEditorWinForm/Form1.cs
@@ -14,7 +14,33 @@ namespace ImageEditorWinForm
     {
         public Form1()
         {
+            Console.Write("Draw");
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            int xCoordinate = Cursor.Position.X;
+            int yCoordinate = Cursor.Position.Y;
+
+            pictureBox1.Image = pictureBox1.Image;
+
+            ((Bitmap)pictureBox1.Image).SetPixel(10, 10, Color.FromArgb(255, 255, 0));
+
+            Console.Write("Draw");
+
+           /* pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            int x, y;
+            for (y = 0; y < 200; y++)
+            {
+                for (x = 0; x < 200; x++)
+                {
+                    ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.FromArgb(255, 255, 0));
+                }
+            }*/
         }
     }
 }
--- ImageEditorWinForm/Program.cs
+++ ImageEditorWinForm/Program.cs
@@ -10,37 +10,12 @@ namespace ImageEditorWinForm
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            Graphics g = pictureBox1.DemonstratePropertyItem();
-        }
-
-        private static void DemonstratePropertyItem(PaintEventArgs e)
-        {
-
-            // Create two images.
-            Image image1 = Image.FromFile("c:\\FakePhoto1.jpg");
-            Image image2 = Image.FromFile("c:\\FakePhoto2.jpg");
-
-            // Get a PropertyItem from image1.
-            PropertyItem propItem = image1.GetPropertyItem(20624);
-
-            // Change the ID of the PropertyItem.
-            propItem.Id = 20625;
-
-            // Set the PropertyItem for image2.
-            image2.SetPropertyItem(propItem);
-
-            // Draw the image.
-            e.Graphics.DrawImage(image2, 20.0F, 20.0F);
         }
     }
 }
