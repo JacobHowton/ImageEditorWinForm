4a961494423706d6786165e6db220327143467c0
JacobHowton <Jacob@howton.com>
2020-07-27 10:26:29 -0500

    Added a fill algorithm and implemented

--- ImageEditorWinForm/Form1.Designer.cs
+++ ImageEditorWinForm/Form1.Designer.cs
@@ -33,6 +33,10 @@
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label1 = new System.Windows.Forms.Label();
             this.pbColorWheel = new System.Windows.Forms.PictureBox();
+            this.btn_pen = new System.Windows.Forms.Button();
+            this.btn_square = new System.Windows.Forms.Button();
+            this.btn_circle = new System.Windows.Forms.Button();
+            this.btn_fill = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbColorWheel)).BeginInit();
@@ -42,11 +46,12 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.pictureBox1.Location = new System.Drawing.Point(24, 16);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(908, 494);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
@@ -54,8 +59,8 @@
             // trackBar1
             // 
             this.trackBar1.LargeChange = 1;
-            this.trackBar1.Location = new System.Drawing.Point(1008, 62);
-            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Location = new System.Drawing.Point(1000, 65);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
             this.trackBar1.Maximum = 50;
             this.trackBar1.Minimum = 1;
             this.trackBar1.Name = "trackBar1";
@@ -67,7 +72,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1044, 42);
+            this.label1.Location = new System.Drawing.Point(1034, 44);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 17);
@@ -78,7 +83,7 @@
             // 
             this.pbColorWheel.Image = ((System.Drawing.Image)(resources.GetObject("pbColorWheel.Image")));
             this.pbColorWheel.Location = new System.Drawing.Point(940, 308);
-            this.pbColorWheel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbColorWheel.Margin = new System.Windows.Forms.Padding(4);
             this.pbColorWheel.Name = "pbColorWheel";
             this.pbColorWheel.Size = new System.Drawing.Size(261, 202);
             this.pbColorWheel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -86,16 +91,62 @@
             this.pbColorWheel.TabStop = false;
             this.pbColorWheel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbColorWheel_MouseClick);
             // 
+            // btn_pen
+            // 
+            this.btn_pen.Location = new System.Drawing.Point(1039, 147);
+            this.btn_pen.Name = "btn_pen";
+            this.btn_pen.Size = new System.Drawing.Size(75, 23);
+            this.btn_pen.TabIndex = 4;
+            this.btn_pen.Text = "Pen";
+            this.btn_pen.UseVisualStyleBackColor = true;
+            this.btn_pen.Click += new System.EventHandler(this.btn_pen_Click);
+            // 
+            // btn_square
+            // 
+            this.btn_square.Location = new System.Drawing.Point(1039, 176);
+            this.btn_square.Name = "btn_square";
+            this.btn_square.Size = new System.Drawing.Size(75, 23);
+            this.btn_square.TabIndex = 5;
+            this.btn_square.Text = "Square";
+            this.btn_square.UseVisualStyleBackColor = true;
+            this.btn_square.Click += new System.EventHandler(this.btn_square_Click);
+            // 
+            // btn_circle
+            // 
+            this.btn_circle.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_circle.Location = new System.Drawing.Point(1039, 205);
+            this.btn_circle.Name = "btn_circle";
+            this.btn_circle.Size = new System.Drawing.Size(75, 23);
+            this.btn_circle.TabIndex = 6;
+            this.btn_circle.Text = "Circle";
+            this.btn_circle.UseVisualStyleBackColor = false;
+            this.btn_circle.Click += new System.EventHandler(this.btn_circle_Click);
+            // 
+            // btn_fill
+            // 
+            this.btn_fill.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_fill.Location = new System.Drawing.Point(1039, 234);
+            this.btn_fill.Name = "btn_fill";
+            this.btn_fill.Size = new System.Drawing.Size(75, 23);
+            this.btn_fill.TabIndex = 7;
+            this.btn_fill.Text = "Fill";
+            this.btn_fill.UseVisualStyleBackColor = false;
+            this.btn_fill.Click += new System.EventHandler(this.btn_fill_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1217, 554);
+            this.Controls.Add(this.btn_fill);
+            this.Controls.Add(this.btn_circle);
+            this.Controls.Add(this.btn_square);
+            this.Controls.Add(this.btn_pen);
             this.Controls.Add(this.pbColorWheel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -112,6 +163,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.PictureBox pbColorWheel;
+        private System.Windows.Forms.Button btn_pen;
+        private System.Windows.Forms.Button btn_square;
+        private System.Windows.Forms.Button btn_circle;
+        private System.Windows.Forms.Button btn_fill;
     }
 }
 
--- ImageEditorWinForm/Form1.cs
+++ ImageEditorWinForm/Form1.cs
@@ -18,13 +18,19 @@ namespace ImageEditorWinForm
         public int drawWidth;
         public Color colorDraw;
 
-        public enum DrawMode {pen, square};
+        public enum DrawMode {pen, square, circle, fill};
         public DrawMode drawMode;
 
         public struct pt
         {
             public int x;
             public int y;
+
+            public pt (int xnew, int ynew)
+            {
+                this.x = xnew;
+                this.y = ynew;
+            }
         };
 
         public pt pt1;
@@ -38,7 +44,7 @@ namespace ImageEditorWinForm
             img = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             drawWidth = trackBar1.Value;
             colorDraw = Color.White;
-            drawMode = DrawMode.square;
+            drawMode = DrawMode.pen;
 
             pictureBox1.Image = img;
         }
@@ -49,7 +55,7 @@ namespace ImageEditorWinForm
             {
                 if (drawMode == DrawMode.pen)
                 {
-                    draw(e.X, e.Y, drawWidth);
+                    draw(e.X, e.Y);
                 }
 
                 pictureBox1.Image = img;
@@ -58,7 +64,7 @@ namespace ImageEditorWinForm
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (drawMode == DrawMode.square)
+            if (drawMode == DrawMode.square || drawMode == DrawMode.circle)
             {
                 pt1.x = e.X;
                 pt1.y = e.Y;
@@ -69,19 +75,32 @@ namespace ImageEditorWinForm
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            if (drawMode == DrawMode.square)
+            if (drawMode == DrawMode.square || drawMode == DrawMode.circle)
             {
                 pt2.x = e.X;
                 pt2.y = e.Y;
 
-                drawSquare(drawWidth);
+                if (drawMode == DrawMode.square)
+                {
+                    drawSquare();
+                }
+
 
                 pictureBox1.Image = img;
             }
             mouseIsDown = false;
         }
 
-        private void draw(int x, int y, int drawWidth)
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (drawMode == DrawMode.fill)
+            {
+                fill(e.X, e.Y);
+            }
+        }
+
+        private void draw(int x, int y)
         {
             for (int width = 0; width < drawWidth; width ++)
             {
@@ -94,36 +113,94 @@ namespace ImageEditorWinForm
                         img.SetPixel(imgx, imgy, colorDraw);
                 }
             }
-            
         }
 
 
-        private void drawSquare(int drawWidth)
+        private void drawSquare()
         {
             int x1 = Math.Min(pt1.x, pt2.x);
             int x2 = Math.Max(pt1.x, pt2.x);
             int y1 = Math.Min(pt1.y, pt2.y);
             int y2 = Math.Max(pt1.y, pt2.y);
 
-            for (int x = x1; x < x2; x ++)
+            for (int x = x1; x <= x2; x += drawWidth)
             {
-                draw(x, y1, drawWidth);
+                draw(x, y1);
             }
 
-            for (int x = x1; x < x2; x++)
+            for (int x = x2; x >= x1; x -= drawWidth)
             {
-                draw(x, y2, drawWidth);
+                draw(x, y2);
             }
 
-            for (int y = y1; y < y2; y++)
+            for (int y = y2; y >= y1; y -= drawWidth)
             {
-                draw(x1, y, drawWidth);
+                draw(x1, y);
             }
 
-            for (int y = y1; y < y2; y++)
+            for (int y = y1; y <= y2; y += drawWidth)
             {
-                draw(x2, y, drawWidth);
+                draw(x2, y);
             }
+        }
+
+        private void drawCircle()
+        {
+
+        }
+
+        private void fill(int x, int y)
+        {
+            Queue<pt> points = new Queue<pt>();
+            pt centerPoint;
+            centerPoint.x = x;
+            centerPoint.y = y;
+
+            if(img.GetPixel(x,y) != colorDraw)
+            {
+                points.Enqueue(centerPoint);
+
+                fillRecursive(points, img.GetPixel(x,y));
+            }
+        }
+
+        private void fillRecursive(Queue<pt> points, Color fillingColor)
+        {
+            pt currentPoint = points.Dequeue();
+
+            img.SetPixel(currentPoint.x, currentPoint.y, colorDraw);
+
+            if (img.GetPixel(currentPoint.x + 1, currentPoint.y) == fillingColor)
+            {
+                points.Enqueue(new pt(currentPoint.x + 1, currentPoint.y));
+            }
+
+            if (img.GetPixel(currentPoint.x - 1, currentPoint.y) == fillingColor)
+            {
+                points.Enqueue(new pt(currentPoint.x - 1, currentPoint.y));
+            }
+
+            if (img.GetPixel(currentPoint.x, currentPoint.y + 1) == fillingColor)
+            {
+                points.Enqueue(new pt(currentPoint.x, currentPoint.y + 1));
+            }
+
+            if (img.GetPixel(currentPoint.x, currentPoint.y - 1) == fillingColor)
+            {
+                points.Enqueue(new pt(currentPoint.x, currentPoint.y - 1));
+            }
+
+            //MessageBox.Show((points.Count).ToString(), "o");
+            
+            if (points.Count != 0)
+            {
+                fillRecursive(points, fillingColor);
+            }
+            else
+            {
+                pictureBox1.Image = img;
+            }
+
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
@@ -137,6 +214,26 @@ namespace ImageEditorWinForm
             int y = pbColorWheel.Image.Height * e.Y / pbColorWheel.Height;
 
             colorDraw = ((Bitmap)pbColorWheel.Image).GetPixel(x, y);
+        }
+
+        private void btn_pen_Click(object sender, EventArgs e)
+        {
+            drawMode = DrawMode.pen;
+        }
+
+        private void btn_square_Click(object sender, EventArgs e)
+        {
+            drawMode = DrawMode.square;
+        }
+
+        private void btn_circle_Click(object sender, EventArgs e)
+        {
+            drawMode = DrawMode.circle;
+        }
+
+        private void btn_fill_Click(object sender, EventArgs e)
+        {
+            drawMode = DrawMode.fill;
         }
     }
 }
